9b06fd4307fa80510f3753b39a4f4d3a9ea0cd34
Ajay Daniel Trevor <ajaydtrevor@gmail.com>
2025-01-07 00:37:42 +0530

    completed

--- app/page.js
+++ app/page.js
@@ -1,101 +1,77 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [totalStudents, setTotalStudents] = useState(57);
+  const [absentees, setAbsentees] = useState([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const findAbsentee = (index) => absentees.find((value) => value == index);
+
+  const studentsArray = Array.from({ length: totalStudents }, (_, i) => i + 1);
+
+  const currentDate = new Date();
+
+  const day = String(currentDate.getDate()).padStart(2, "0"); // Add leading zero if needed
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // Month is zero-based
+  const year = currentDate.getFullYear();
+
+  const formattedDate = `${day}-${month}-${year}`;
+
+  const copyText = () => {
+    const textToCopy = `Today's Absentees\nDate: ${formattedDate}\nAbsentees: ${absentees.join(
+      ", "
+    )}`;
+    navigator.clipboard.writeText(textToCopy);
+  };
+  return (
+    <div className="flex flex-col items-center p-4 py-6">
+      <div className="max-w-sm w-full">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="totalStudents">Total Students</Label>
+          <Input
+            type="number"
+            onChange={(e) => setTotalStudents(e.target.value)}
+            value={totalStudents}
+            id="totalStudents"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="flex flex-wrap gap-4 my-10 max-w-xl">
+        {studentsArray.map((student) => (
+          <div
+            key={student}
+            className={`w-20 h-20 border border-black flex items-center justify-center ${
+              findAbsentee(student)
+                ? "bg-red-500 text-white"
+                : "bg-white text-black"
+            }`}
+            onClick={() =>
+              setAbsentees(
+                (prevState) =>
+                  prevState.includes(student)
+                    ? prevState.filter((name) => name !== student) // Remove if exists
+                    : [...prevState, student] // Add if not exists
+              )
+            }
+          >
+            {student}
+          </div>
+        ))}
+      </div>
+
+      {absentees.length > 0 && (
+        <div className="max-w-sm w-full flex flex-col gap-2">
+          <h2>{`Today's Absentees`}</h2>
+          <h3>Date : {formattedDate}</h3>
+          <h3>Absentees : {absentees.join(", ")}</h3>
+          <Button onClick={copyText}>Copy text</Button>
+        </div>
+      )}
     </div>
   );
 }
